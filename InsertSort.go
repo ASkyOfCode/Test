48d8d71df3d4f819057c51d5bc24ed46e0e4d0a8
Unity <1292749184@qq.com>
2016-07-17 17:31:54 +0800

    This is InsertSort.go

---
+++ InsertSort.go
@@ -0,0 +1,26 @@
+package main;
+import "fmt"
+func InsertSort(array []int){
+    for i := 0;i<len(array);i++ {
+	    for j := 0;j<i;j++ { 
+	        if array[j] > array[i] {
+		   temp := array[i]
+		   index := i;
+		   for x := 0;x<i-j;x++ {
+			   array[index],array[index-1]=array[index-1],array[index]
+		           index--
+		   }
+		   array[j] = temp
+		   break
+	      }
+           }
+    }  
+}
+func main(){
+   var array = []int{54,85,34,26,2,58,64,33,14,56}
+   InsertSort(array)
+   for _,ch := range array{
+	  fmt.Printf("%d  ",ch)
+   }
+   fmt.Printf("\n")
+}
