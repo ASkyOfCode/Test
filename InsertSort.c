39973b2e5d4c5e604241d53bf69802376a32af4d
Unity <1292749184@qq.com>
2016-07-21 17:02:04 +0800

    This is InsertSort.c

--- InsertSort.c
+++ InsertSort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void InsertSort(int *array,int length){
   for(int i=0;i<length;i++){
-	  for(int j=0;j<i;j++){
+	 /* for(int j=0;j<i;j++){
 	    if(array[j] > array[i]){
 		    int temp = array[i];
 		    int index = i;
@@ -12,7 +12,14 @@ void InsertSort(int *array,int length){
 		    array[j] = temp;
 		    break;
 	     }
-         } 
+         }*/
+	  int j =i;
+	  int temp = array[i];
+	  while(j>0 && temp < array[j-1]){
+		  array[j] = array[j-1];
+		  j--;
+	  }
+	  array[j] = temp;
    }
 }
 void main(){
